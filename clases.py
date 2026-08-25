2bac1bd67ef510e1d1b32eb7e731bf79d4cde17d
jsantamarina <jsantamarina@itba.edu.ar>
2026-08-25 18:44:41 -0300

    agregue clase credencial

---
+++ clases.py
@@ -0,0 +1,16 @@
+from datetime import date
+from typing import List, Set
+
+
+class Credencial:
+    """Modela una credencial profesional con su período de vigencia."""
+
+    def __init__(self, nombre: str, fecha_obtencion: date, fecha_expiracion: date):
+        self.nombre = nombre
+        self.fecha_obtencion = fecha_obtencion
+        self.fecha_expiracion = fecha_expiracion
+
+    def esta_activa(self, fecha_consulta: date):
+        """Regla 2: Determina si la credencial está activa en una fecha específica."""
+        return self.fecha_obtencion <= fecha_consulta <= self.fecha_expiracion
+
